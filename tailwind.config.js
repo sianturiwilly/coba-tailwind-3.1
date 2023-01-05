3dc70ef286aa3f241226d46709de74621f02d472
sianturiwilly <prajudiwilliam10@gmail.com>
2023-01-05 17:21:21 +0700

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ['index.html'],
+  theme: {
+    extend: {
+      colors: {
+        hijau: '#bada55',
+        tema: 'rgb(var(--warna-tema) / <alpha-value>)',
+      },      
+    },
+  },
+  plugins: [],
+};
